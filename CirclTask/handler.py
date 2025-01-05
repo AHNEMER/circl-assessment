4da5ea2fad7f1692ef19b221a243ca16c1645c4c
‪ahmad alnemer‬‏ <ahnemerah@gmail.com>
2025-01-05 23:33:01 +0300

    adding comment

--- CirclTask/handler.py
+++ CirclTask/handler.py
@@ -6,6 +6,8 @@ from botocore.exceptions import ClientError
 s3 = boto3.client('s3')
 bucket_name = os.environ['S3_BUCKET_NAME']
 #jsjsjsjsjsjsjs
+#jsjsjsjsjsjsjs
+
 def uploadCustomer(event, context):
     try:
         # Parse the JSON body from the incoming event
